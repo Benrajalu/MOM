6d9ec3d006f2a24c89beb1283a840de9d8cafa1d
Benoit Rajalu <rajalubenoit@gmail.com>
2014-10-23 14:45:00 +0200

    Password retrival pop-in

--- source/js/plugins.js
+++ source/js/plugins.js
@@ -27,4 +27,9 @@ jQuery(window).load(function () {
         manualControls : '#sliderNav li'
     });
 
+    $('.pop').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
+
 });
